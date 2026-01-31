d6ca2f95a468e964cfd102f32918819fc4c0e001
Vadzim Bardziyer <bardiervadim97@gmail.com>
2026-01-31 19:36:43 +0300

    split main to schemas and prompt, refactoring

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from typing import Any, cast
 from dotenv import load_dotenv
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
@@ -14,29 +18,13 @@ load_dotenv()
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
 
-template = """Answer the following questions as best you can. You have access to the following tools:
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names", "tools"],
+).partial(format_instructions=output_parser.get_format_instructions())
 
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}"""
-
-react_prompt = PromptTemplate.from_template(template)
-
-agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt_with_format)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 chain = agent_executor
 
