0fd54d9003425c19eaabb223ff81c85c886fe1f6
Vadzim Bardziyer <bardiervadim97@gmail.com>
2026-02-01 16:21:43 +0300

    implement AgentExecutor with logs

--- main.py
+++ main.py
@@ -1,41 +1,103 @@
-from typing import Any, cast
-
 from dotenv import load_dotenv
-from langchain_classic.agents import AgentExecutor
-from langchain_classic.agents.react.agent import create_react_agent
+from langchain.tools import tool
+from langchain_classic.agents.output_parsers import \
+    ReActSingleInputOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
+from langchain_core.tools import BaseTool, render_text_description
 from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
 
-from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
-from schemas import AgentResponse
+from callbacks import AgentCallbackHandler
+from log import format_log_to_str
 
 load_dotenv()
 
 
-tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4")
-structured_llm = llm.with_structured_output(AgentResponse)
-react_prompt_with_format = PromptTemplate(
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables=["input", "agent_scratchpad", "tool_names", "tools"],
-).partial(format_instructions="")
+@tool
+def get_text_length(text: str) -> int:
+    """Return the length of a text by characters."""
+    print(f"get_text_length called with: {text}")
+    text = text.strip("'\n").strip('"')
+    return len(text)
 
-agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt_with_format)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-extract_output = RunnableLambda(lambda x: x["output"])
-chain = agent_executor | extract_output | structured_llm
+
+def find_tool_by_name(tools: list[BaseTool], tool_name: str) -> BaseTool | ValueError:
+    for t in tools:
+        if t.name == tool_name:
+            return t
+    return ValueError(f"Tool with name {tool_name} not found")
 
 
 def main():
-    print("Hello from lc course")
-    result = chain.invoke(
-        input={
-            "input": "Search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-        }
+    print("Hello from ReAct LangChain!")
+    tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought: {agent_scratchpad}
+    """
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
     )
-    print(result)
+
+    llm = ChatOpenAI(
+        temperature=0,
+        model="gpt-4",
+        stop_sequences=["\nObservation", "Observation:", "Observation"],
+        callbacks=[AgentCallbackHandler()],
+    )
+
+    intermediate_steps = []
+
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
+        }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
+    )
+
+    agent_step: AgentAction | AgentFinish | str = ""
+
+    while not isinstance(agent_step, AgentFinish):
+        agent_step: AgentAction | AgentFinish = agent.invoke(
+            {
+                "input": "What is the length of 'DOG' in characters?",
+                "agent_scratchpad": intermediate_steps,
+            }
+        )
+        print(agent_step)
+
+        if isinstance(agent_step, AgentAction):
+            tool_to_use = find_tool_by_name(tools, agent_step.tool)
+            tool_input = agent_step.tool_input
+
+            observation = tool_to_use.func(str(tool_input))
+            print(f"{observation}")
+            intermediate_steps.append((agent_step, str(observation)))
+
+    if isinstance(agent_step, AgentFinish):
+        print(agent_step.return_values)
 
 
 if __name__ == "__main__":
---
+++ react_agent_old.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
+load_dotenv()
+
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
+structured_llm = llm.with_structured_output(AgentResponse)
+react_prompt_with_format = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names", "tools"],
+).partial(format_instructions="")
+
+agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt_with_format)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+chain = agent_executor | extract_output | structured_llm
+
+
+def main():
+    print("Hello from lc course")
+    result = chain.invoke(
+        input={
+            "input": "Search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
