52d42f84d39b3fbe9f58753c338ba721721c8412
Vadzim Bardziyer <bardiervadim97@gmail.com>
2026-02-16 12:13:14 +0300

    Add grader to RAG agent

--- corrective_RAG/graph/chains/tests/test_chains.py
+++ corrective_RAG/graph/chains/tests/test_chains.py
@@ -3,6 +3,10 @@ from pprint import pprint
 from dotenv import load_dotenv
 
 from corrective_RAG.graph.chains.generation import generation_chain
+from corrective_RAG.graph.chains.hallucination_grader import (
+    GradeHallucinations,
+    hallucination_grader,
+)
 from corrective_RAG.graph.chains.retrieval_grader import (
     GradeDocuments,
     retrieval_grader,
@@ -40,3 +44,27 @@ def test_generation_chain() -> None:
     docs = retriever.invoke(question)
     generation = generation_chain.invoke({"question": question, "context": docs})
     pprint(generation)
+
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"question": question, "context": docs})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we newed to start with the dog",
+        }
+    )
+    assert not res.binary_score
--- corrective_RAG/graph/graph.py
+++ corrective_RAG/graph/graph.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from langgraph.graph import END, StateGraph
 
+from corrective_RAG.graph.chains.answer_grader import answer_grader
+from corrective_RAG.graph.chains.hallucination_grader import hallucination_grader
 from corrective_RAG.graph.consts import GENERATE, GRADE_DOCUMENTS, RETRIEVE, WEBSEARCH
 from corrective_RAG.graph.nodes import generate, grade_documents, retrieve, web_search
 from corrective_RAG.graph.state import GraphState
@@ -19,6 +21,33 @@ def decide_to_generate(state: GraphState) -> str:
         return GENERATE
 
 
+def grader_generation_grounded_in_documents_and_question(state: GraphState) -> str:
+    print("---Check HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    hallucination_score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+
+    if hallucination_grade := hallucination_score.binary_score:
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        print("---GRADE GENERATION vs QUESTION---")
+        answer_score = answer_grader.invoke(
+            {"question": question, "generation": generation}
+        )
+        if answer_grade := answer_score.binary_score:
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESSES QUESTION---")
+            return "not userful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE_TRY---")
+        return "not supported"
+
+
 workflow = StateGraph(GraphState)
 workflow.add_node(RETRIEVE, retrieve)
 workflow.add_node(GRADE_DOCUMENTS, grade_documents)
@@ -32,6 +61,13 @@ workflow.add_conditional_edges(
     decide_to_generate,
     {WEBSEARCH: WEBSEARCH, GENERATE: GENERATE},
 )
+
+workflow.add_conditional_edges(
+    GENERATE,
+    grader_generation_grounded_in_documents_and_question,
+    {"useful": END, "not useful": WEBSEARCH, "not supported": GENERATE},
+)
+
 workflow.add_edge(WEBSEARCH, GENERATE)
 workflow.add_edge(GENERATE, END)
 
